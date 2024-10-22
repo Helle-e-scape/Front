d6134356268afb5aeedf6fad7e6fa7bc849b7761
Safouane <tebibe42@gmail.com>
2024-10-22 10:17:20 +0200

    Résolution de conflits

--- src/navigation/AuthNavigationStack.jsx
+++ src/navigation/AuthNavigationStack.jsx
@@ -1,23 +1,61 @@
 // src/navigation/AuthNavigationStack.jsx
-import React from "react";
+import React, { useEffect } from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import Login from "../screens/Login";
 import Room from "../screens/Room";
+import { WebSocketProvider } from "../context/WebSocketContext";
+import { UserProvider } from "../context/UserContext";
 import WaitingRoom from "../screens/WaitingRoom";
+import Game from "../screens/Game";
+import Scoreboard from "../screens/Scoreboard";
+import Trap from "../screens/Trap"; // Import de l'écran Trap
 
 const Stack = createStackNavigator();
 
-const AuthNavigationStack = () => {
+const AuthNavigationStack = ({ CustomText, CustomTextInput }) => {
   return (
+    <UserProvider>
+    <WebSocketProvider>
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen
         name="Login"
         component={Login}
         options={{ title: "Login" }}
+        initialParams={{ CustomText, CustomTextInput }}
       />
-      <Stack.Screen name="Room" component={Room} options={{ title: "Room" }} />
-      <Stack.Screen name="Waiting Room" component={WaitingRoom} options={{title: "Waiting Room"}}/>
+      <Stack.Screen
+        name="Room"
+        component={Room}
+        options={{ title: "Room" }}
+        initialParams={{ CustomText, CustomTextInput }}
+      />
+      <Stack.Screen
+        name="WaitingRoom"
+        component={WaitingRoom}
+        options={{ title: "WaitingRoom" }}
+        initialParams={{ CustomText, CustomTextInput }}
+      />
+      <Stack.Screen
+        name="Game"
+        component={Game}
+        options={{ title: "Game" }}
+        initialParams={{ CustomText, CustomTextInput }}
+      />
+      <Stack.Screen
+        name="Scoreboard"
+        component={Scoreboard}
+        options={{ title: "Scoreboard" }}
+        initialParams={{ CustomText, CustomTextInput }}
+      />
+      <Stack.Screen
+        name="Trap"
+        component={Trap} // Ajout de l'écran Trap
+        options={{ title: "Trap" }}
+        initialParams={{ CustomText, CustomTextInput }}
+      />
     </Stack.Navigator>
+    </WebSocketProvider>
+    </UserProvider>
   );
 };
 
