2dcba4373a9109a5787ecefa7469d034936233c3
Safouane <tebibe42@gmail.com>
2024-10-23 17:00:39 +0200

    Corrected all conflicts + added better screen logic + player list with a callAPI in the waiting room

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -42,7 +42,7 @@ const PixelButton = ({ title, onPress }) => {
   return (
     <View style={styles.container}>
       <View style={styles.buttonContainer}>
-        <TouchableOpacity onPress={handleOnPress} disabled={isPressed}>
+        <TouchableOpacity onPress={handleOnPress} disabled={isPressed}/>
         <TouchableOpacity onPress={() => {onPress(), handlePress()}} disabled={isPressed}>
           <Image
             source={
