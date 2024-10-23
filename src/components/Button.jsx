c96eb9d704ea41e79f783a331e45de1c5f524ce2
Safouane <tebibe42@gmail.com>
2024-10-23 17:15:43 +0200

    Corrected some bugs in the screen logic

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -24,7 +24,8 @@ const PixelButton = ({ title, onPress }) => {
   }, 250);
 
   const handleOnPress = async () => {
-    const result = await onPress(); 
+    const result = await onPress();
+    console.log(result); 
 
     if (result === true) {  
       handlePress();  
@@ -42,8 +43,7 @@ const PixelButton = ({ title, onPress }) => {
   return (
     <View style={styles.container}>
       <View style={styles.buttonContainer}>
-        <TouchableOpacity onPress={handleOnPress} disabled={isPressed}/>
-        <TouchableOpacity onPress={() => {onPress(), handlePress()}} disabled={isPressed}>
+        <TouchableOpacity onPress={handleOnPress} disabled={isPressed}>
           <Image
             source={
               isPressed
