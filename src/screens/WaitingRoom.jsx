d435aa1367521865cd14ff744ae2c4b2eeb4291c
Safouane <tebibe42@gmail.com>
2024-10-29 12:08:00 +0100

    Fixed grid selection and limitations

--- src/screens/WaitingRoom.jsx
+++ src/screens/WaitingRoom.jsx
@@ -19,7 +19,6 @@ const WaitingRoom = () => {
 
 useEffect(() => {
   const fetchPlayerList = async () => {
-    navigation.navigate("Grid");
     try {
       const response = await authApi.findByIdRoom(user.roomId);
       setPlayerList(response.users); // Remplir la liste avec les utilisateurs récupérés via l'API
